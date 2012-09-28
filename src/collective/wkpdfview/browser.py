298bcc038c67eb24139bbaa081e0483d14f86e4c
Silvio Tomatis <silviot@gmail.com>
2012-09-28 17:35:50 +0200

    Build wkhtmltopdf compatible cookiejar file with the same cookies we werfe passed in the request

--- src/collective/wkpdfview/browser.py
+++ src/collective/wkpdfview/browser.py
@@ -1,6 +1,8 @@
+import os
 from subprocess import check_call, PIPE
 from tempfile import mktemp
-import os
+from urlparse import urlparse
+
 
 class WKPdfView(object):
     def __init__(self, context, request):
@@ -14,13 +16,27 @@ class WKPdfView(object):
         if '/@@wkpdf' not in url:
             raise RuntimeError("This use case is not implemented yet")
         url = url.replace('/@@wkpdf', '')
-        filepath = mktemp()
-        result = check_call((command, url, filepath),
-                            stdout=PIPE, stderr=PIPE)
-        if result == 0:
+        filepath = mktemp('.pdf')
+        cookiepath = build_cookiejar(self.request)
+
+        try:
+            check_call((command, '--cookie-jar', cookiepath, url, filepath),
+                        stdout=PIPE, stderr=PIPE)
             self.request.response.setHeader("Content-type", "application/pdf")
             with open(filepath) as fh:
                 filecontents = fh.read()
             return filecontents
-        else:
-            raise RuntimeError("Error running wkhtmltopdf")
+        finally:
+            if os.path.isfile(filepath):
+                os.unlink(filepath)
+            if os.path.isfile(cookiepath):
+                os.unlink(cookiepath)
+
+
+def build_cookiejar(request):
+    u = urlparse(request.URL)
+    filename = mktemp('.jar')
+    with open(filename, 'w') as fh:
+        for k, v in request.cookies.items():
+            fh.write("%s=%s; domain=%s; path=/;\n" % (k, v, u.hostname))
+    return filename
--- src/collective/wkpdfview/tests/test_wkpdfview.py
+++ src/collective/wkpdfview/tests/test_wkpdfview.py
@@ -1,14 +1,20 @@
 import unittest2 as unittest
+import transaction
 
 from Products.CMFCore.utils import getToolByName
 
-from urllib import urlopen
+from plone.app.testing import login, setRoles, TEST_USER_NAME
+from plone.app.testing import TEST_USER_ID, TEST_USER_PASSWORD
+
+from urllib2 import urlopen, build_opener
+from urllib import quote
+from base64 import encodestring
 
 from collective.wkpdfview.testing import\
     COLLECTIVE_WKPDFVIEW_FUNCTIONAL
 
 
-class TestExample(unittest.TestCase):
+class TestWkPdfView(unittest.TestCase):
 
     layer = COLLECTIVE_WKPDFVIEW_FUNCTIONAL
 
@@ -26,8 +32,8 @@ class TestExample(unittest.TestCase):
         self.assertTrue(pid in installed,
                         'package appears not to have been installed')
 
-    def test_download_homepage_as_pdf(self):
-        portal = self.layer['portal']
+    def test_download_anonymous_homepage_as_pdf(self):
+        portal = self.portal
         path = '/'.join(portal.getPhysicalPath())
         host, port = self.layer['zserver_info']
         url = "http://%(host)s:%(port)i%(path)s/@@wkpdf" % locals()
@@ -35,3 +41,28 @@ class TestExample(unittest.TestCase):
         pdf_data = result.read()
         # PDFs have magic byte %PDF
         self.assertEqual('%PDF', pdf_data[:4])
+
+    def test_download_logged_in_page_as_pdf(self):
+        # We create a (private) page
+        setRoles(self.portal, TEST_USER_ID, ['Manager'])
+        login(self.portal, TEST_USER_NAME)
+        self.portal.invokeFactory('Document', id='doc')
+        transaction.commit() # We're on a different ZODB connection than urllib
+        doc = self.portal.doc
+        path = '/'.join(doc.getPhysicalPath())
+        host, port = self.layer['zserver_info']
+        url = "http://%(host)s:%(port)i%(path)s/@@wkpdf" % locals()
+        # Build login cookie
+        cookie = quote(encodestring(
+            '%s:%s' % (TEST_USER_NAME.encode('hex'), TEST_USER_PASSWORD.encode('hex'))
+        ).rstrip())
+        opener = build_opener()
+        opener.addheaders.append(('Cookie', "__ac=%s" % cookie))
+
+
+        result = opener.open(url)
+        pdf_data = result.read()
+        # PDFs have magic byte %PDF
+        self.assertEqual('%PDF', pdf_data[:4])
+        # XXX TODO Check (probably with pdfquery)
+        # that our real content is in the PDF, not just an error page
