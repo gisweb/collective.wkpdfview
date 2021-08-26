e56a444dccbdb7eff44186c8ad82a8fa5a13c695
Roberto Starnini <roberto.starnini@gmail.com>
2021-08-26 08:55:31 +0200

    base auth

--- src/collective/wkpdfview/browser.py
+++ src/collective/wkpdfview/browser.py
@@ -4,7 +4,10 @@ from subprocess32 import CalledProcessError
 from tempfile import mktemp
 from urlparse import urlparse
 import json
-
+try:
+    from .user import user
+except:
+    user=''
 
 class WKPdfView(object):
     def __init__(self, context, request):
@@ -27,6 +30,7 @@ class WKPdfView(object):
             path = path + '?' + self.request.get('QUERY_STRING')
         url = 'http://%s:%s%s' % (host, port, path)
         filepath = mktemp('.pdf')
+        
         scriptpath = make_scriptfile(self.request, host)
         try:
             try:
@@ -56,7 +60,7 @@ def make_scriptfile(request, hostname=None):
         cookiestr = json.dumps(dict(cookie, name=k, value=v))
         addcookie_scripts.append("phantom.addCookie(%s);" % cookiestr)
     with open(path, 'w') as fh:
-        fh.write(RASTERIZE_SCRIPT % '\n'.join(addcookie_scripts))
+        fh.write(RASTERIZE_SCRIPT % ('\n'.join(addcookie_scripts), user))
     return path
 
 
@@ -73,6 +77,7 @@ if (phantom.args.length < 2 || phantom.args.length > 3) {
     %s
     page.paperSize = {format: "A4"};
     page.viewportSize = { width: 600, height: 600 };
+    page.customHeaders={'Authorization': 'Basic '+btoa('%s')};
     page.open(address, function (status) {
         if (status !== 'success') {
             console.log('Unable to load the address!');
